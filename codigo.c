7b242a337c024c241f237402f7120ddbd821e6f9
Danilo0liveira <danilo.melo@ee.ufcg.edu.br>
2026-02-01 23:45:21 -0300

    acho que ta certo

--- codigo.c
+++ codigo.c
@@ -5,10 +5,14 @@
 #include <unistd.h>
 
 #define NUM_MACHINES 2
-#define BUFFER_SIZE_MACHINE 2
+#define BUFFER_SIZE_MACHINE 1
 #define BUFFER_SIZE_OUT 2
 
-int buffer_m[2];
+#define MACHINE_MAX_TIME 10
+#define ROBOT_MAX_TIME 1
+#define CONVEYOR_MAX_TIME 5
+
+int buffer_m[NUM_MACHINES];
 int buffer_out = 0;
 
 sem_t s, mutex, full;
@@ -17,12 +21,14 @@ sem_t m[NUM_MACHINES];
 void *machine(void *arg)
 {
     int id = *(int *)arg;
+    
     while(1)
     {
-        sleep(rand() % 3 + 1);
-        buffer_m[id]+=1;
-        sem_post(&s);
+        sleep(rand() % MACHINE_MAX_TIME + 1);
         sem_wait(&m[id]);
+        buffer_m[id]+=1;
+        printf("\ngerando produto machine %d %d", id, buffer_m[id]);
+        sem_post(&s);
     }
 }
 
@@ -30,21 +36,23 @@ void *robot(void *arg)
 {
     while(1)
     {
-        sleep(rand() % 3 + 1);
+        sleep(rand() % ROBOT_MAX_TIME + 1);
         sem_wait(&s);
-
+        
         for(int i = 0; i<NUM_MACHINES; i++)
         {
             if (buffer_m[i])
             {
                 buffer_m[i]-=1;
                 sem_post(&m[i]);
+                printf("\npegando produto machine %d %d", i, buffer_m[i]);
                 break;
             }
         }
         sem_wait(&full);
         sem_wait(&mutex);
         buffer_out+=1;
+        printf("\nbufferout %d", buffer_out);
         sem_post(&mutex);
     }
 }
@@ -53,22 +61,53 @@ void *conveyor(void *arg)
 {
     while(1)
     {
-        sleep(rand() % 3 + 1);
+        sleep(rand() % CONVEYOR_MAX_TIME + 1);
         sem_wait(&mutex);
+        printf("\nbufferout %d", buffer_out);
         if (buffer_out>0)
+        {
             buffer_out-=1;
             sem_post(&full);
+        }
         sem_post(&mutex);
     }
 }
 
 int main()
 {
-    sem_init(&s, 0, NUM_MACHINES-1);
-    sem_init(&mutex, 0, 0);
-    sem_init(&full, 0, BUFFER_SIZE_OUT-1);
+    srand(time(NULL));
 
-    for (int i = 0; i < NUM_MACHINES; i++){
+    sem_init(&s, 0, 0);
+    sem_init(&mutex, 0, 1);
+    sem_init(&full, 0, BUFFER_SIZE_OUT);
+
+    for (int i = 0; i < NUM_MACHINES; i++)
         sem_init(&m[i], 0, BUFFER_SIZE_MACHINE);
+       
+    pthread_t machines_t[NUM_MACHINES];
+    pthread_t robot_t;
+    pthread_t conveyor_t;
+
+    int const_ids[NUM_MACHINES];
+
+    for (int i = 0; i < NUM_MACHINES; i++)
+    {
+        const_ids[i] = i;
+        pthread_create(
+            &machines_t[i], NULL, machine, &const_ids[i]);
     }
+    
+    pthread_create(&robot_t, NULL, robot, NULL);
+    pthread_create(&conveyor_t, NULL, conveyor, NULL);
+    
+    for (int i = 0; i < NUM_MACHINES; i++)
+        pthread_join(machines_t[i], NULL);
+        
+    pthread_join(robot_t, NULL);
+    pthread_join(conveyor_t, NULL);
+
+    sem_destroy(&s);
+    sem_destroy(&mutex);
+    sem_destroy(&full);
+    
 }
